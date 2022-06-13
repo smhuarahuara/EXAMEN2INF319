dc60b2a974b485fec9f5fe6bb5d012de60604787
smhuarahuara <77683748+smhuarahuara@users.noreply.github.com>
2022-06-13 02:18:37 -0400

    Add files via upload

---
+++ PREGUNTA4/Library1.fs
@@ -0,0 +1,32 @@
+﻿namespace Library1
+
+module fibos =
+    let generafibo n =
+        let mutable s=""
+        let mutable a = 0
+        let mutable b = 1
+        let mutable c = 1
+        for i in 0..n do
+           s <- s+" "+a.ToString()
+           a <- b
+           b <- c
+           c <- a + b
+        s
+    let rec fib n =
+        if n=0 then 0
+        elif n=1 then 1
+        elif n=2 then 1
+        else fib (n-1) + fib (n-2)
+
+    let genera n =
+        let mutable s = ""
+        for i in 0..n do
+            s<-s+" "+ (fib i).ToString()
+        s
+
+module calcu =
+    let suma x y = x + y
+    let mutli x y = x* y
+
+type Class1() = 
+    member this.X = "F#"
---
+++ PREGUNTA4/WebService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Descripción breve de WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+// [System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService {
+
+    public WebService () {
+
+        //Elimine la marca de comentario de la línea siguiente si utiliza los componentes diseñados 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld() {
+        return "Hola a todos";
+    }
+
+    [WebMethod]
+    public string fibrec(int a)
+    {
+        return Library1.fibos.genera(a);
+    }
+
+    [WebMethod]
+    public string fibest(int a)
+    {
+        return Library1.fibos.generafibo(a);
+    }
+    
+
+}
