5dc717c4e0bd81dd71c890cec5053cc9e101042b
smhuarahuara <77683748+smhuarahuara@users.noreply.github.com>
2022-06-13 12:00:31 -0400

    Update fibo_recursiva.hs

--- PREGUNTA1/fibo_recursiva.hs
+++ PREGUNTA1/fibo_recursiva.hs
@@ -1,16 +1,16 @@
 module Main where
 
-  fib :: Integer -> Integer
-  fib 0 = 0
-  fib 1 = 1
-  fib 2 = 1
-  fib n = fib (n-1) + fib (n-2)
+  fibo :: Integer -> Integer
+  fibo 0 = 0
+  fibo 1 = 1
+  fibo 2 = 1
+  fibo n = fibo (n-1) + fibo (n-2)
 
   fibonacci :: Integer -> Integer -> String
-  fibonacci n fibMax =
-    if n < fibMax-1 then
-      show (fib n) ++ " " ++ fibonacci (succ n) fibMax
+  fibonacci n nMax =
+    if n < nMax-1 then
+      show (fibo n) ++ " " ++ fibonacci (succ n) nMax
     else
-      show (fib n)
+      show (fibo n)
 
   main = print (fibonacci 0 5)
