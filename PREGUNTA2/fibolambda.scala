3b960f512d67fc4a6befa74e053dc343b3cae3df
smhuarahuara <77683748+smhuarahuara@users.noreply.github.com>
2022-06-14 13:27:48 -0400

    Update fibolambda.scala

--- PREGUNTA2/fibolambda.scala
+++ PREGUNTA2/fibolambda.scala
@@ -1,12 +1,12 @@
 object tempEstruc{ 
 // Main method 
    def main(args:Array[String]):Unit={  
+      var b=1 
+      var b1=1 
+      var c=0
       val x=8 
       var i=0 
       var a=0 
-      var b=1 
-      var b1=1 
-      var c=0    
       val fib2 = (n:Int) =>  
       while( i < n ) { 
          c=a+b 
@@ -24,7 +24,7 @@ object tempEstruc{
          println(a) 
          i=i+1 
       } 
-      //println(fib2(x)) 
+      println(fib2(x)) 
       println(fib3(x)) 
    } 
 }
